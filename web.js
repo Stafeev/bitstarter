53ca05be5b9d4085f8a2c4b7294614ec005668f0
Stafeev <dunlop300g@yandex.ru>
2013-07-15 20:15:14 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,17 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+   var buf=new Buffer(5);
+   var fs=require("fs");
+   fs.readFileSync('index.html', function read(err,data)
+     {
+      if (err) {
+        throw err;
+        }
+       buf=data;    
+      });
+  var str=buf.toString("utf-8");
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
